fa820be224f3299882a5a8ec8fea81183aca63ee
Andrew Hu <andrewhu32009@gmail.com>
2026-06-26 17:45:35 -0400

    Initial Commit

---
+++ Zenly/ZenlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenlyApp.swift
+//  Zenly
+//
+//  Created by Andrew Hu on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct ZenlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZenlyTests/ZenlyTests.swift
@@ -0,0 +1,19 @@
+//
+//  ZenlyTests.swift
+//  ZenlyTests
+//
+//  Created by Andrew Hu on 6/26/26.
+//
+
+import Testing
+@testable import Zenly
+
+struct ZenlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
